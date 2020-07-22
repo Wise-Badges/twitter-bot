717d390ae600a7e0dd921281e5e7083ffdc563eb
Ward Beyens <45149049+wardbeyens@users.noreply.github.com>
2020-07-22 20:04:12 +0200

    added twitter logic

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 require("dotenv").config();
 const { Autohook } = require("twitter-autohook");
-const fs = require('fs');
-
+const fs = require("fs");
+const axios = require("axios");
 
 function isMention(event) {
   return "user_has_blocked" in event ? true : false;
+}
+
+function isLike(event) {
+  return "favorite_events" in event ? true : false;
+}
+
+function getSenderName(tweet) {
+  return tweet.tweet_create_events[0].user.name;
+}
+
+function getSenderUrl(tweet) {
+  return "https://twitter.com/" + tweet.tweet_create_events[0].user.screen_name;
+}
+
+function getSender(tweet) {
+  return {
+    senderName: tweet.tweet_create_events[0].user.name,
+    senderUrl:
+      "https://twitter.com/" + tweet.tweet_create_events[0].user.screen_name,
+  };
+}
+
+function getBadge(tweet) {
+  const badge = tweet.tweet_create_events[0].entities.hashtags[0].text;
+  return badge;
+}
+
+function getReceiver(tweet) {
+  const user_mentions = tweet.tweet_create_events[0].entities.user_mentions;
+
+  //could be just an if statement
+  /*   for (i = 0; i < user_mentions.length; i++) {
+    if (user_mentions[i].id_str !== process.env.TWITTER_BOT_ID_STR) {
+      return {
+        receiverName: user_mentions[i].name,
+        receiverUrl: "https://twitter.com/" + user_mentions[i].screen_name,
+      };
+    }
+       return ;
+
+  } */
+
+  if (user_mentions[0].id_str == process.env.TWITTER_BOT_ID_STR) {
+    return {
+      receiverName: user_mentions[1].name,
+      receiverUrl: "https://twitter.com/" + user_mentions[1].screen_name,
+    };
+  } else {
+    return {
+      receiverName: user_mentions[0].name,
+      receiverUrl: "https://twitter.com/" + user_mentions[0].screen_name,
+    };
+  }
+}
+
+function getTweetUrl(tweet) {
+  return getSenderUrl(tweet) + "/status/" + tweet.tweet_create_events[0].id_str;
 }
 
 (async (start) => {
@@ -15,7 +72,7 @@ function isMention(event) {
       consumer_key: process.env.TWITTER_CONSUMER_KEY,
       consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
       env: process.env.TWITTER_WEBHOOK_ENV,
-      port: 5000,
+      port: process.env.PORT,
     });
 
     // Removes existing webhooks
@@ -37,13 +94,28 @@ function isMention(event) {
       // if (isMention(event)) {
       //   console.log("----- DIT IS EEN MENTION -----");
       // }
-      if (!isMention(event)) {
-        return;
+      if (isMention(event)) {
+        console.log("--- mention ---");
+        // console.log(event);
+        // fs.writeFileSync("mention3.json", JSON.stringify(event));
+
+        axios
+          .post("https://postman-echo.com/post", filterData(event))
+          .then(function (response) {
+            // console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+
+
+          
+      } else if (isLike(event)) {
+        console.log("--- like ---");
+        console.log(event);
+        fs.writeFileSync("like.json", JSON.stringify(event));
       }
-
-      console.log(event);
-      fs.writeFileSync('mention2.json', JSON.stringify(event));
-
     });
   } catch (e) {
     // Display the error and quit
--- wip.js
+++ wip.js
@@ -1,41 +1,151 @@
-var fs = require('fs');
-const axios = require('axios');
+const Twit = require('twit'); 
+var fs = require("fs");
+var axios = require("axios");
+require("dotenv").config();
 
+const twit = new Twit({
+  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+  access_token:         process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
+}); 
 
 let globalData;
 
 try {
-    const data = fs.readFileSync('mention2.json', 'utf8');
-    // console.log(data);    
-    globalData = data;
-} catch(e) {
-    console.log('Error:', e.stack);
+  const data = fs.readFileSync("mention2.json", "utf8");
+  // console.log(data);
+  globalData = data;
+} catch (e) {
+  console.log("Error:", e.stack);
 }
 
-function getSenderName(tweet){
-    return tweet.tweet_create_events[0].user.name
+function getSenderName(tweet) {
+  return tweet.tweet_create_events[0].user.name;
 }
 
-function getSenderUrl(tweet){
-    return 'https://twitter.com/' + tweet.tweet_create_events[0].user.screen_name
+function getSenderUrl(tweet) {
+  return "https://twitter.com/" + tweet.tweet_create_events[0].user.screen_name;
 }
 
-function getBadge(tweet){
-
-    return 'https://twitter.com/' + tweet.tweet_create_events[0].user.screen_name
+function getSender(tweet) {
+  return {
+    senderName: tweet.tweet_create_events[0].user.name,
+    senderUrl:
+      "https://twitter.com/" + tweet.tweet_create_events[0].user.screen_name,
+  };
 }
 
+async function getBadgeclasses() {
+  const badgeclasses = await axios.get(process.env.API_URL + "/badgeclasses");
+  const response = badgeclasses.data.badgeclasses;
+  return response;
+  // I want this function to return the JSON data Array
+}
 
+async function getBadge(tweet) {
+  const tweetBadgeTag = tweet.tweet_create_events[0].entities.hashtags[0].text;
 
-function filterData(event){
-    // console.log(event)
-    console.log(getSenderName(event))
-    console.log(getSenderUrl(event))
-    
-    console.log(event.tweet_create_events[0].extended_tweet.entities.hashtags[0].text)
+  const response = await getBadgeclasses();
 
+  for (i = 0; i < response.length; i++) {
+    if (response[i].tag.toLowerCase() === tweetBadgeTag.toLowerCase()) {
+      const badgeID = response[i].id;
+      console.log(badgeID);
+      return badgeID;
+    }
+  }
+  return Error;
+}
+
+function getReceiver(tweet) {
+  const user_mentions = tweet.tweet_create_events[0].entities.user_mentions;
+
+  if (user_mentions[0].id_str == process.env.TWITTER_BOT_ID_STR) {
+    return {
+      receiverName: user_mentions[1].name,
+      receiverUrl: "https://twitter.com/" + user_mentions[1].screen_name,
+    };
+  } else {
+    return {
+      receiverName: user_mentions[0].name,
+      receiverUrl: "https://twitter.com/" + user_mentions[0].screen_name,
+    };
+  }
+}
+
+function getTweetUrl(tweet) {
+  return getSenderUrl(tweet) + "/status/" + tweet.tweet_create_events[0].id_str;
+}
+
+async function filterData(event) {
+  return {
+    receiver: getReceiver(event).receiverUrl,
+    receiverName: getReceiver(event).receiverName,
+    sender: getSenderUrl(event),
+    senderName: getSenderName(event),
+    platform: "twitter",
+    senderUrl: getSenderUrl(event),
+    reason: getTweetUrl(event),
+    badgeclass: await getBadge(event),
+  };
+}
+
+function sendTweet(rec, sen, tag, html, img) {
+  const msg =
+    "Congratulations @" +
+    rec +
+    "! @" +
+    sen +
+    " issued you a #" +
+    tag +
+    ". Now cherish and embrace your WiseBadge here: " +
+    html;
+
+    var params = { status: msg };
+
+    twit.post('statuses/update', params, function (err, data, response) {
+      console.log(data);
+      console.log("TWITTER RESPONSE "+JSON.stringify(response));
+   });
 
 
 }
 
-filterData(JSON.parse(globalData))
+// ----------------------------------------------
+
+const webhookMention = JSON.parse(globalData);
+filterData(webhookMention)
+  .then(async (data) => {
+    // console.log(data);
+    const receiver_screen_name = /[^/]*$/.exec(data.receiver)[0];
+    const sender_screen_name = /[^/]*$/.exec(data.sender)[0];
+
+    const responsePost = await axios.post(
+      process.env.API_URL + "/assertion",
+      data
+    );
+    // console.log(responsePost.data);
+    const responsePostHtml = responsePost.data.html;
+    const responsePostJson = responsePost.data.json;
+
+    const responseAssertion = await axios.get(responsePostJson);
+    // console.log(responseAssertion.data);
+
+    const responseBadgeclass = await axios.get(responseAssertion.data.badge);
+    // console.log(responseBadgeclass.data);
+    const responseBadgeclassTag = responseBadgeclass.data.tag;
+    const responseBadgeclassImage = responseBadgeclass.data.image;
+
+    sendTweet(
+      receiver_screen_name,
+      sender_screen_name,
+      responseBadgeclassTag,
+      responsePostHtml,
+      responseBadgeclassImage
+    );
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
